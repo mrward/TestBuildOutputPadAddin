d57e20fe61193de07831061caff3bb973f134be1
Matt Ward <matt.ward@microsoft.com>
2023-05-18 12:41:02 +0100

    Create test addin

---
+++ TestBuildOutputPadAddin/Properties/AddinInfo.cs
@@ -0,0 +1,10 @@
+﻿using Mono.Addins;
+
+[assembly: Addin(
+    "TestBuildOutputPad",
+    Namespace = "Microsoft.VisualStudioMac.Extensibility.Samples",
+    Version = "0.1",
+    Category = "IDE extensions")]
+
+[assembly: AddinName("Test Build Output Pad")]
+[assembly: AddinDescription("Test Build Output Pad")]
